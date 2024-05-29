82f01e1564134c3055eff9f8cc57a20cf7cf668f
venu-04 <kamsuvenu2004@gmail.com>
2024-05-29 13:05:58 +0530

    hii

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import  { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
  const [formData, setFormData] = useState({
@@ -61,6 +62,7 @@ const handleSubmit = async (event) => {
             <input type="password" id="password" name="password" placeholder="Enter Password" required className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value={formData.password} onChange={handleChange} />
           </div>
           <button type="submit" className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign Up</button>
+          <p className="mt-4 text-center text-gray-500">Already have an account? <Link to="/signin" className="text-indigo-500 hover:text-indigo-700">Sign In</Link></p>
         </form>
       </div>
     </div>
