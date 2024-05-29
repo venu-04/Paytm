162197a7db34fdc647b15a08f518fb7dc66232b4
venu-04 <kamsuvenu2004@gmail.com>
2024-05-29 14:01:53 +0530

    m1

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,22 @@
-import express from 'express';
-import rootrouter from './routes/server.js';
+import express from "express";
+import rootrouter from "./routes/server.js";
 import cors from 'cors';
-
 const app = express();
 
-const corsOptions = {
-    origin: 'https://paytm-eta-umber.vercel.app/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors({
+    origin: 'https://paytm-eta-umber.vercel.app'
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', rootrouter);
+app.use('/',rootrouter);
 
-app.options('*', cors(corsOptions)); // Handle preflight requests
-
-app.get("/", (req, res) => {
+app.get("/",(req,res) => {
     console.log("Hello World");
     res.send("Hello World");
+
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(8000,()=>{
+    console.log("server is running on port 8000");
+})
+
